6a2f076172417d9097522cd09076a9c0329e91c1
your name <huy.db.67khmt@ntu.edu.vn>
2025-11-17 12:25:24 +0700

    tính diện tích hình chữ nhật

---
+++ tinh-dien-tich-va-chu-vi-hinh-CN.py
@@ -0,0 +1,10 @@
+x,y=7,7
+if x!=y:
+    print("x=",x)
+    print("y=",y)
+    print("chu vi hình chữ nhật")
+    print("S=",x*y)
+    print("chu vi hinh chu nhat")
+    print("D=",2*x+2*y)
+else:
+    print(" khong phai hinh chu nhat")
