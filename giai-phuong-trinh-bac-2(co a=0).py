ac33aba1d165678318fa9d451e92ce708e9486f9
your name <huy.db.67khmt@ntu.edu.vn>
2025-11-19 00:15:41 +0700

    Create giai-phuong-trinh-bac-2(co a=0).py

---
+++ giai-phuong-trinh-bac-2(co a=0).py
@@ -0,0 +1,28 @@
+# giair phuong trinh bac 2 co (a=0)
+print("a**2*x+b*x+c=0")
+a=int(input("nhap a= "))
+b=int(input("nhap b= "))
+c=int(input("nhap c= "))
+print(a,"**2*x+",b,"*x",c,"=0")
+if a!=0:
+    delta=a**2+4*a*c
+    import math
+    dellta=math.sqrt(delta)
+    if delta>0:
+        print("phuong trinh co 2 nghiem")
+        x1=(-b-dellta)/2*a
+        x2=(-b+dellta)/2*a
+        print("x1= ",x1)
+        print("x2= ",x2)
+    else:
+        if delta==0:
+            print("phuong trinh co nghiem kep")
+            x=-b/(2*a)
+            print("x= ")
+        else:
+             print("phuong trinh vo nghiem")
+else:                   
+    print("phuong trinh bac nhat")
+    x=-c/b
+    print("nghiem x= ",x)
+        
