5f03ef23da4d222aecf8bf8d343d6f7d6d274449
your name <huy.db.67khmt@ntu.edu.vn>
2025-12-01 01:48:06 +0700

    Create in-cap-so-nha.py

---
+++ in-cap-so-nha.py
@@ -0,0 +1,33 @@
+#in bang chu chuong
+print("cap nhan so 2")
+for i in range(1,11):
+    d=2*i
+    print("2*",i,"=",d)
+print("cap nhan so 3")
+for i in range(1,11):
+    e=3*i
+    print("3*",i,"=",e)
+print("cap nhan so 4")    
+for i in range(1,11):
+    a=4*i
+    print("4*",i,"=",a)
+print("cap nhan so 5")    
+for i in range(1,11):
+    p=5*i 
+    print("5*",i,"=",p)
+print("cap nhan so 6")
+for i in range(1,11):
+    o=6*i
+    print("6*",i,"=",o)
+print("cap nhan so 7")
+for i in range(1,11):
+    y=7*i 
+    print("7*",i,"=",y)
+print("cap nhan so 8")
+for i in range(1,11):
+    k=8*i
+    print("8*",i,"=",k)
+print("cap nha so 9")
+for i in range(1,11):
+    g=9*i
+    print("9*",i,"=",g)
