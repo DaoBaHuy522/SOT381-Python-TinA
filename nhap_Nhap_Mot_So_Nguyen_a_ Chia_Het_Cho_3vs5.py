feb9872f5c806c19140af85a25021fcaf5c687da
your name <huy.db.67khmt@ntu.edu.vn>
2025-12-08 13:56:07 +0700

    Create nhap_Nhap_Mot_So_Nguyen_a_ Chia_Het_Cho_3vs5.py

---
+++ nhap_Nhap_Mot_So_Nguyen_a_ Chia_Het_Cho_3vs5.py
@@ -0,0 +1,8 @@
+#viet nhap 1 so nguyen cho biet co cho het dong thoi 3 va 5 hay ko
+a=int(input(f"nhap so a "))
+h=a%3
+k=a%5
+if h==0:
+    print("chia het cho3")
+if k==0:
+    print(" chia het cho5")
