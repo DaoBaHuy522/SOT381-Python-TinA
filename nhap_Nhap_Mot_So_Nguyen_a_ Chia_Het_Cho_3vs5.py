7bcb812c1a4316c46f0d51ae56978342cf079851
your name <huy.db.67khmt@ntu.edu.vn>
2025-12-08 14:00:33 +0700

    Update nhap_Nhap_Mot_So_Nguyen_a_ Chia_Het_Cho_3vs5.py

--- nhap_Nhap_Mot_So_Nguyen_a_ Chia_Het_Cho_3vs5.py
+++ nhap_Nhap_Mot_So_Nguyen_a_ Chia_Het_Cho_3vs5.py
@@ -2,7 +2,10 @@
 a=int(input(f"nhap so a "))
 h=a%3
 k=a%5
-if h==0:
-    print("chia het cho3")
-if k==0:
-    print(" chia het cho5")
+if h==0 or k==0:
+    print("ca 3 va 5 deu chia het")
+else:
+    if h==0:
+       print("chia het cho3")
+    if k==0:
+       print(" chia het cho5")
