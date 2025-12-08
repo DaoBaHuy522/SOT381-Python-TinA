6aeee204b095621bf94d5ed9ddc620e513080193
your name <huy.db.67khmt@ntu.edu.vn>
2025-12-08 15:55:37 +0700

    Create tinh_Dai_Thua_Mu_N.py

---
+++ tinh_Dai_Thua_Mu_N.py
@@ -0,0 +1,8 @@
+a=int(input("nhap so a= "))
+b=int(input("nhap so dai thua ban dau b= "))
+c=int(input("nhap so dai thua cuoi c="))
+for i in range(b-1,c):
+    h=a**i
+    print(f"so dai thua cua la={h} ")
+    
+    
