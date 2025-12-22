0227eb59b91ddaf28dce91cca60a3780d62cd1bc
your name <huy.db.67khmt@ntu.edu.vn>
2025-12-22 14:38:47 +0700

    Update TH2212_Bai1.py

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -1,7 +1,14 @@
 # Viết chương trình nhập vào độ dài 2 cạnh
-a=int(input("nhap do dai canh: "))
-b=int(input("nhap chieu rong canh: "))
+while True:
+    w=float(input("nhap do dai canh: "))
+    h=float(input("nhap chieu rong canh: "))
+    if w>=0 and w<=100:
+      break
+    else:
+        print(f"nhap da sai")
 chu_vi=2*a+2*b
 dien_tich=a*b
 print(f"chu vi hinh chu nhat la {chu_vi:.2f}")
 print(f"dien tich hinh chu nhat {dien_tich:.2f}")
+        
+        
