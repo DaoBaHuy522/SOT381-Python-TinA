760abc6ee1090912babcc859ff50de07312ad043
your name <huy.db.67khmt@ntu.edu.vn>
2025-12-22 14:06:53 +0700

    Create TH2212_Bai1.py

---
+++ TH2212_Bai1.py
@@ -0,0 +1,7 @@
+# Viết chương trình nhập vào độ dài 2 cạnh
+a=int(input("nhap do dai canh: "))
+b=int(input("nhap chieu rong canh: "))
+chu_vi=2*a+2*b
+dien_tich=a*b
+print(f"chu vi hinh chu nhat la {chu_vi:.2f}")
+print(f"dien tich hinh chu nhat {dien_tich:.2f}")
